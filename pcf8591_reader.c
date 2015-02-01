43154c4929598f0434c2016357558c5d3bdf66cd
bmayer <brett.mayer00@gmail.com>
2015-02-01 18:14:06 +0000

    good to go

--- pcf8591_reader.c
+++ pcf8591_reader.c
@@ -1,8 +1,8 @@
-#include <stdio.h>
-#include <wiringPi.h>
-#include <pcf8591.h>
+# include <stdio.h>
+# include <wiringPi.h>
+# include <pcf8591.h>
 
-#define PINBASE 120 //must be > 64
+# define PINBASE 120 //must be > 64
 
 //read the analogue input from a potentiometer
 
@@ -11,12 +11,12 @@ int main(void) {
   int pin = 0; //ain0
   double pin_val;
 
-  wiringPiSetupGpio();
+  wiringPiSetupGpio();  // enable bcm pin numbering
   pcf8591Setup(PINBASE, 0x48);
 
-  while(1)
-  {
-    pin_val = analogRead(pin);
+  while(1) {
+    pin_val = analogRead(PINBASE + pin) / 2.55; //analog range 0 - 255, convert to 0 - 100
+    pin_val = (pin_val == 0) ? pin_val = 1 : pin_val; //pwm doesnt allow 0
     printf("pin_val: %5.2f\n", pin_val);
     delay(500); //ms
   }
