bc633332cb5f1c989243e15a6dc821abda02d38d
bmayer <brett.mayer00@gmail.com>
2015-02-21 20:34:19 +0000

    added logic for in3/4 pins

--- interrupt_test2.c
+++ interrupt_test2.c
@@ -8,12 +8,19 @@
 #include <softPwm.h>
 #include <pcf8591.h>
 
-#define ENB0 19
+#define ENB0 19 //enable/pwm b motor
 #define ENB1 26
+#define IN3 5 //motor directoin
+#define IN4 6 //motor direction
 #define JSL 20 //joystick input left
 #define JSR 21 //joystick input right
 #define RANGE 100  //pwm range
 #define PINBASE 120  //pcf pinbase
+
+/*
+compile with:
+gcc -Wall -o interrupt_test2 interrupt_test2.c -lwiringPi -lpthread
+*/
 
 /*
 test by running the following:
@@ -33,15 +40,20 @@ int main (void) {
   wiringPiSetupGpio();
   pinMode(ENB0, OUTPUT); //sw pwm
   pinMode(ENB1, OUTPUT);
+  pinMode(IN3, OUTPUT); //b motor direction
+  pinMode(IN4, OUTPUT);
+
   digitalWrite(ENB0, 0); //init as low
   digitalWrite(ENB1, 0); //init as low
+  digitalWrite(IN3, 0); //init as low
+  digitalWrite(IN4, 0); //init as low
   //pinMode(ENB1, PWM_OUTPUT); //if using hw pwm
   pinMode(JSL, INPUT);
   pinMode(JSR, INPUT);
   digitalWrite(JSL, 0); //init as low
   digitalWrite(JSR, 0); //init as low
 
-  //softPwmCreate(ENB0, 1, RANGE); //pin, initial value, range
+  softPwmCreate(ENB0, 1, RANGE); //pin, initial value, range
   softPwmCreate(ENB1, 1, RANGE); //pin, initial value, range
 
   pcf8591Setup(PINBASE, 0x48);
@@ -63,6 +75,7 @@ void start_pwm(void) {
   //printf("start_pwm...\n");
   //pwm_freq is read from AIN0
   int pwm_freq;
+
   while (1) {
     //test if JOYSTICK is low
     if (digitalRead(JSL) == 0 && digitalRead(JSR) == 0) {
@@ -71,14 +84,34 @@ void start_pwm(void) {
       digitalWrite(ENB1, 0);
       break;
     }
-
+    //test if JOYSTICK is left high
+    else if (digitalRead(JSL) == 1) {
+      //printf("\rjoystick left");
+      //fflush(stdout);
+      //digitalWrite(ENB0, 1);
+      //digitalWrite(ENB1, 1);
+      digitalWrite(IN3, 0);
+      digitalWrite(IN4, 1);
+    }
+    //test if JOYSTICK is right high
+    else if (digitalRead(JSR) == 1) {
+      //printf("\rjoystick right");
+      //fflush(stdout);
+      //digitalWrite(ENB0, 1);
+      //digitalWrite(ENB1, 1);
+      digitalWrite(IN3, 1);
+      digitalWrite(IN4, 0);
+    }
+  
     pwm_freq = analogRead(PINBASE + 0) / 2.55; //AIN0
+
     if (pwm_freq == 0) {
       pwm_freq = 1;
     }
+
     printf("\rpwm: %d", pwm_freq);
     fflush(stdout);
-    //softPwmWrite(ENB0, pwm_freq); //writing pwm freq to L298N
+    softPwmWrite(ENB0, pwm_freq); //writing pwm freq to L298N
     softPwmWrite(ENB1, pwm_freq); //writing pwm freq to L298N
     //pwmWrite(); //hw pwm bcm port 18 range 0-1024
     //digitalWrite(ENB0, 1);
